37c57bd4d7c364b0cecacd55c28edf5eed9ced61
MennatAllah <mennamahamed3@gmail.com>
2026-01-15 14:11:00 +0200

    Implement Dashboard Layout

--- js/login.js
+++ js/login.js
@@ -1,21 +1,26 @@
 const form = document.querySelector(".login-form form")
+const loginEmail = document.querySelector(".login-email-input")
 const loginPass = document.querySelector(".login-pass-input")
 const showPassIcon = document.querySelector(".show-pass-icon")
 const hidePassIcon = document.querySelector(".login-form .hide-pass-icon")
-const submitBtn = document.querySelector(".submit-btn");
 
-form.addEventListener("submit", () => {
-    submitBtn.disabled = true
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+  if(loginEmail.value.trim() !== "" && loginPass.value.trim() !== "") {
+    e.preventDefault()
+    window.location.href = "dashboard.html"
+    window.localStorage.setItem("username", JSON.stringify({username: loginEmail.value, isLogged: true}))
+  }
 })
 showPassIcon.addEventListener("click", () => {
-    showPassIcon.classList.add("d-none")
-    loginPass.type = 'text'
-    hidePassIcon.classList.remove("d-none")
+  showPassIcon.classList.add("d-none")
+  loginPass.type = 'text'
+  hidePassIcon.classList.remove("d-none")
 })
 hidePassIcon.addEventListener("click", () => {
-    hidePassIcon.classList.add("d-none")
-    loginPass.type = 'password'
-    showPassIcon.classList.remove("d-none")
+  hidePassIcon.classList.add("d-none")
+  loginPass.type = 'password'
+  showPassIcon.classList.remove("d-none")
 })
 
 const responsive = gsap.matchMedia();
